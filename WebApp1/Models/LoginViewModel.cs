0081d5ed41183604dcccdd92ddca6202d98e7f5f
Biswadeep Dey <dey.biswadeep@outlook.com>
2023-08-03 00:51:29 +0530

    modified the layout

---
+++ WebApp1/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
